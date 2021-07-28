d31e724f1765ced73c7d8b0602b53559fdd4b836
kravatka <yegorkrava@gmail.com>
2021-07-29 00:15:34 +0300

    add api documentation url

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('documentation', function () {
+    return 'docs text';
+});
+
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
